4572f2c2420cf3e7e4db71046b5f66224380f524
JacobEscoto <djacobescotoz@gmail.com>
2026-04-21 21:45:46 -0600

    add handling and status messages for submission of contact form

--- js/script.js
+++ js/script.js
@@ -3,3 +3,48 @@ const copyrightContent = document.getElementById("copyright");
 if (copyrightContent) {
   copyrightContent.textContent = `© ${new Date().getFullYear()} Jacob Escoto`;
 }
+
+const contactForm = document.getElementById("contact-form");
+const status = document.getElementById("form-status");
+
+contactForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  const submitBtn = document.getElementById("submit-btn");
+
+  submitBtn.textContent = "Sending...";
+  submitBtn.disabled = true;
+
+  try {
+    const response = await fetch("https://formspree.io/f/mojyjbao", {
+      method: "POST",
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (response.ok) {
+      status.innerHTML = "Thanks! Your message have been sent.";
+      status.style.color = "rgb(62, 194, 255)";
+      contactForm.reset();
+
+      setTimeout(() => {
+        status.style.opacity = "0";
+
+        setTimeout(() => {
+          status.innerHTML = "";
+          status.style.opacity = "1";
+        }, 500);
+      }, 5000);
+    } else {
+      status.innerHTML = "Oops! There was an error while sending.";
+    }
+  } catch (error) {
+    status.innerHTML = "Connection error. Try again later!";
+  } finally {
+    submitBtn.textContent = "Send";
+    submitBtn.disabled = false;
+  }
+});
